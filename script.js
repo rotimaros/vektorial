24596e07ab399e68db39b4ad23d5b43086621ae7
Abd. Rahim Saleh <rahim.birbal@gmail.com>
2020-01-26 00:28:50 +0800

    penambahan content

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-  //sliders
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
-  const faders = document.querySelectorAll(".fade-in");
-  const sliders = document.querySelectorAll(".slide-in");
-  
-  const appearOptions = {
-      threshold: 0,
-      rootMargin: "0px 0px -250px 0px"
-    };
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
   
   const appearOnScroll = new IntersectionObserver(function(
-      entries,
-      appearOnScroll
-    ) {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          return;
-        } else {
-          entry.target.classList.add("appear");
-          appearOnScroll.unobserve(entry.target);
-        }
-      });
-    },
-    appearOptions);
-    
-    faders.forEach(fader => {
-      appearOnScroll.observe(fader);
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
     });
-    
-    sliders.forEach(slider => {
-      appearOnScroll.observe(slider);
-    });
-    
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
+  
